2b830c357d1a904cf25aea3323d72924149a58e5
Retr0327 <philcoke35@gmail.com>
2022-08-19 00:44:15 +0800

    Add YearBackwardIndexParser

---
+++ scraptt/spiders/utils/parsers/html_index/__init__.py
@@ -0,0 +1,6 @@
+from .index import IndexParser
+from .latest_index import LatestIndexParser
+from .year_backward_index import YearBackwardIndexParser
+
+
+__all__ = ["IndexParser", "LatestIndexParser", "YearBackwardIndexParser"]
