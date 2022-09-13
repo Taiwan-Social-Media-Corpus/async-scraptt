f5d04c74a45179538e6278230d221f789ed8af5a
Retr0327 <philcoke35@gmail.com>
2022-09-14 02:27:11 +0800

    Add download_ckip_drivers

--- scraptt/configs/__init__.py
+++ scraptt/configs/__init__.py
@@ -1,4 +1,5 @@
-from .configs import ckip_path, download_ckip_drivers, COOKIES
+from .ckip import download_ckip_drivers
+from .urls import PTT, PTT_BOARD, COOKIES
 
 
-__all__ = ["ckip_path", "download_ckip_drivers", "COOKIES"]
+__all__ = ["download_ckip_drivers", "PTT", "PTT_BOARD", "COOKIES"]
