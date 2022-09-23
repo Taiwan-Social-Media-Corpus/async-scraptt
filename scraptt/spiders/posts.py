c28d33d997b48c2971b32ecccf6b24516b35a362
Retr0327 <philcoke35@gmail.com>
2022-09-24 01:47:06 +0800

    Update PttSpider

--- scraptt/spiders/posts.py
+++ scraptt/spiders/posts.py
@@ -1,23 +1,62 @@
+import re
+import asyncio
 from ..items import PostItem
 from .base import BasePostSpider
-from .utils.parsers.posts import get_post_data
 from scrapy.http.response.html import HtmlResponse
+from .utils.parsers.posts.meta import get_meta_data
+from .utils.parsers.posts.content import ContentCleaner
+from .utils.parsers.posts.comment import count_comments, CommentsParser
 
 
-class PostsSpider(BasePostSpider):
+async def get_post_info(response: HtmlResponse):
+    return await asyncio.gather(
+        *[
+            get_meta_data(response),
+            count_comments(response),
+            CommentsParser(response).parse(),
+        ]
+    )
+
+
+class PttSpider(BasePostSpider):
     """
-    The PostsSpider object defines the behaviour for crawling and parsing pages for the ptt website.
+    The PttSpider object defines the behaviour for crawling and parsing pages for the ptt website.
     """
 
-    name = "ptt_post"
+    name = "ptt"
 
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
 
-    def parse_post(self, response: HtmlResponse):
-        post_data = get_post_data(response)
+    def parse(self, response: HtmlResponse):
+        main_content = response.dom("#main-content")
 
-        if not post_data:
+        if not main_content:
             return None
 
-        yield PostItem(**post_data).dict()
+        body = ContentCleaner(main_content).clean()
+
+        if body is None:
+            return None
+
+        post_url = response.url
+        board = re.search(r"www\.ptt\.cc\/bbs\/([\w\d\-_]{1,30})\/", post_url).group(1)
+        post_id = post_url.split("/")[-1].split(".html")[0]
+        timestamp = re.search(r"(\d{10})", response.url).group(1)
+
+        meta_header, comment_counter, comments = asyncio.run(get_post_info(response))
+        post_title = meta_header["標題"] if meta_header["標題"] else ""
+        post_author = meta_header["作者"] if meta_header["作者"] else ""
+
+        data = {
+            "board": board,
+            "post_id": post_id,
+            "post_time": timestamp,
+            "title": post_title,
+            "author": post_author,
+            "body": body,
+            "post_vote": comment_counter,
+            "comments": comments,
+        }
+
+        yield PostItem(**data).dict()
