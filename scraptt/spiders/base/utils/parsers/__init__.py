7e3438e27f55cc640c95bf1bd7f7d78ac817c884
Retr0327 <philcoke35@gmail.com>
2023-03-17 19:29:43 +0800

    Export parsers

---
+++ scraptt/spiders/base/utils/parsers/__init__.py
@@ -0,0 +1,5 @@
+from .index import parse_index
+from .latest_index import parse_latest_index
+from .year_range_index import parse_year_range_index
+
+__all__ = ["parse_index", "parse_latest_index", "parse_year_range_index"]
